0bc15f0d5b77cffa74d7f61cdffbf83439f8ce72
vCentDev <vicentDev@yahoo.com>
2026-06-12 18:10:08 +0200

    chore: Remove placeholder content and styles from app.component.html, implementing a clean template for the application.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'menu-manager';
+  protected readonly title = signal('Menú Manager');
 }
